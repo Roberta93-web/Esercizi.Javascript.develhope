d028c98563bc62486587f4aa93f8408e5894d69f
Roberta93-web <federicoroberta1@gmail.com>
2023-01-14 17:53:50 +0100

    esercizio 3 js

---
+++ es-13/arrow.js
@@ -0,0 +1,9 @@
+const sum = (a, b) => { return a + b;}
+
+const subtract = (a, b) => { return a - b;}
+
+const multiply = (a, b) => { return a * b;}
+
+const divide = function (a,b) { return a / b;}
+
+const log = (value) => { console.log(value);}
